a2a87021ab16547ef44af81167a3f4a9331238b8
Tainá Nascimento Sehn Garcia <tainasehn2006@gmail.com>
2026-03-29 14:09:06 -0400

    home.js

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,14 +1,21 @@
-var NoticiasDAO=require('../models/NoticiasDAO');
+var NoticiasDAO = require('../models/NoticiasDAO');
 
-module.exports.index=function(app, req, res){
+module.exports.index = function(app, req, res){
 
-	var connection=app.config.dbConnection();
+	var connection = app.config.dbConnection();
 
 	var noticiasModel = new NoticiasDAO(connection);
 
 	noticiasModel.get5UltimasNoticias(function(error, result){
 
-		res.render('home/home.ejs', {noticias:result});
+		if (error) {
+			console.log('ERRO NA HOME:', error);
+			return res.render('home/home.ejs', { noticias: [] });
+		}
+
+		console.log('RESULTADO HOME:', result);
+
+		res.render('home/home.ejs', { noticias: result });
 
 	});
 
