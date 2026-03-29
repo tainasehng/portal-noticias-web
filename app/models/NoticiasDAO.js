72da1535390b40f1aad7db04511a483a1c911cf8
Tainá Nascimento Sehn Garcia <tainasehn2006@gmail.com>
2026-03-29 12:24:52 -0400

    NoticiasDAO.js

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,23 +1,27 @@
 function NoticiasDAO(connection){
-	this.getNoticias=function(connection,callback){
-		connection.query('select * from noticias', callback);
-	}
 
-	this.getNoticia=function(connection, callback){
-		connection.query('select * from noticias where id_noticia=2', callback);
-	}
+    this.getNoticias = function(callback){
+        connection.query('select * from noticias', callback);
+    }
 
-	this.salvarNoticia=function(noticia, connection, callback){
-		connection.query('insert into noticias set ?', noticia, callback);
-	}
+    this.getNoticia = function(id_noticia, callback){
+        connection.query(
+            'select * from noticias where id_noticia = ?',
+            [id_noticia],
+            callback
+        );
+    }
 
-	this.get5UltimasNoticias=function(connection, callback){
-		connection.query('select * from noticias order by data_criacao desc limit 5', callback);
-	}
+    this.salvarNoticia = function(noticia, callback){
+        connection.query('insert into noticias set ?', noticia, callback);
+    }
+
+    this.get5UltimasNoticias = function(callback){
+        connection.query(
+            'select * from noticias order by data_criacao desc limit 5',
+            callback
+        );
+    }
 }
 
-module.exports=NoticiasDAO;
-
-//Agora que transformamos o NoticiasModel em uma classe (NoticiasDAO), o módulo consign não consegue
-//carregar automaticamente o módulo (NoticiasDAO). Qual a soloção? 
-//Usar require para efetuar a importação do NoticiasDAO.
+module.exports = NoticiasDAO;
