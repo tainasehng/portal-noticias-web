401aa7793d2a287c9391be977d00ced00645ba77
Tainá Nascimento Sehn Garcia <tainasehn2006@gmail.com>
2026-03-29 14:23:53 -0400

    admin.js

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,33 +1,36 @@
-var NoticiasDAO=require('../models/NoticiasDAO');
+var NoticiasDAO = require('../models/NoticiasDAO');
 
 module.exports.formulario_inclusao_noticia = function(app, req, res){
-	res.render('admin/form_add_noticia.ejs', {validacao:{}, noticia:{}});
+	res.render('admin/form_add_noticia.ejs', { validacao: [], noticia: {} });
 };
-	
+
 module.exports.noticias_Salvar = function(app, req, res){
 
-	var noticia=req.body;
+	var noticia = req.body;
 
 	req.assert('titulo', 'Título é obrigatório').notEmpty();
 	req.assert('resumo', 'Resumo é obrigatório').notEmpty();
 	req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10,100);
 	req.assert('autor', 'Autor é obrigatório').notEmpty();
-	req.assert('data_noticia', 'Data é obrigatório').notEmpty();
-	req.assert('noticia', 'Notícia é obrigatório').notEmpty();
+	req.assert('data_noticia', 'Data é obrigatória').notEmpty();
+	req.assert('noticia', 'Notícia é obrigatória').notEmpty();
 
 	var erros = req.validationErrors();
 
 	if(erros){
-		res.render("admin/form_add_noticia.ejs", {validacao : erros}, {noticia : noticia});
+		res.render('admin/form_add_noticia.ejs', { validacao: erros, noticia: noticia });
 		return;
 	}
 
-	var connection=app.config.dbConnection();
-	var noticiasModel= new NoticiasDAO(connection);
+	var connection = app.config.dbConnection();
+	var noticiasModel = new NoticiasDAO(connection);
+
 	noticiasModel.salvarNoticia(noticia, function(error, result){
 		if (error){
 			console.log(error);
+			return res.render('admin/form_add_noticia.ejs', { validacao: [], noticia: noticia });
 		}
+
 		res.redirect('/noticias');
 	});
 }
